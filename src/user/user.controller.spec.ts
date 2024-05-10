e2b172249af5f76ce789eaae8119662c3efb50bf
Stefania Santimbrean <stefaniasantimbrean5@gmail.com>
2024-05-10 23:44:38 +0300

    started to add controller testing

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -4,17 +4,44 @@ import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
+
+  const createUserDto = {
+    email: 'george.bluth@reqres.in',
+    first_name: 'George',
+    last_name: 'Bluth',
+    avatar: 'https://reqres.in/img/faces/1-image.jpg',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            create: jest.fn().mockResolvedValue(createUserDto),
+            findOne: jest.fn().mockResolvedValue({ id: 1, ...createUserDto }),
+            findAvatar: jest.fn(),
+            removeAvatar: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a user', () => {
+    expect(controller.create(createUserDto)).resolves.toEqual(
+      expect.objectContaining(createUserDto),
+    );
+    expect(service.create).toHaveBeenCalled();
+    expect(service.create).toHaveBeenCalledWith(createUserDto);
   });
 });
